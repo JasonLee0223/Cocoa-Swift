d08821166321b6af076aaca02fd0286a884274ed
JasonLee0223 <92699723+JasonLee0223@users.noreply.github.com>
2021-11-10 17:59:59 +0900

    Add files via upload
    연습문제 1~3까지 해결

---
+++ 2Week_Monday/2Week_Monday/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  2Week_Monday
+//
+//  Created by 이건행 on 2021/11/10.
+//
+
+import Foundation
+/*
+// 연습문제1 - 2차원배열 Mission1 호출
+myArray.printArray(array: myArray.fillArray())
+
+
+// 연습문제1 - setBoolArray 와 printArray
+boolArr.printArray(boolOfArray: boolArr.setBoolArray(), newChar: "$")
+*/
+
+// 연습문제1 - 마방진
+//resMagic.printMagicSquare(square: resMagic.makeMagicSquare(row: 3))
+resMagic.printMagicSquare(square: resMagic.makeMagicSquare(row: 5))
