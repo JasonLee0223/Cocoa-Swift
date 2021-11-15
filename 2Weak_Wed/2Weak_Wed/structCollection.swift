1dbef19e01b1c9bbcf2247946a0985627598ee4c
JasonLee0223 <92699723+JasonLee0223@users.noreply.github.com>
2021-11-15 22:08:46 +0900

    Add files via upload
    연습문제1 이자율 계산 구조체 풀이완료

---
+++ 2Weak_Wed/2Weak_Wed/structCollection.swift
@@ -0,0 +1,23 @@
+//
+//  structCollection.swift
+//  2Weak_Wed
+//
+//  Created by 이건행 on 2021/11/15.
+//
+
+
+/*
+import Foundation
+
+struct MyStruct {
+    private var myProperty: Int
+    
+    init() {
+        myProperty = -1
+    }
+    
+    mutating func updateProperty(newValue: Int){
+        myProperty = newValue
+    }
+}
+*/
