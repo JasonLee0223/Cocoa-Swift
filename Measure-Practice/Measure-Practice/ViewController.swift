a20af954721eecde951d054db92ab131d4e3ce7a
JasonLee0223 <92699723+JasonLee0223@users.noreply.github.com>
2021-11-17 09:57:43 +0900

    Add files via upload
    수정하여 재업로드 풀이완료

--- Measure-Practice/Measure-Practice/ViewController.swift
+++ Measure-Practice/Measure-Practice/ViewController.swift
@@ -9,26 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var inputNum: UITextField!
-    @IBOutlet weak var resultValue: UILabel!
-    // weak var는 시스템 내부에서 자체적으로 메모리에서 제거할 수 있음
-    // var는 앱 종료전까지 메모리가 제거되지 않음
-    //@IBOutlet weak var
+    @IBOutlet weak var inputValue: UITextField!
+    @IBOutlet weak var printLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func ConvertToUnit(_ sender: UIButton) {
-        if let inputValue = Double(inputNum.text!)
-        {
-            /*
-             let measureValues = Measurement(value: inputValue, unit: UnitLength.meters)
-                .converted(to: <#T##UnitLength#>.inches)
-        */}
+    @IBAction func convert(_ sender: Any) {
+      
+        guard let userInputValue = inputValue.text, userInputValue.count > 0 else{
+            return
+        } //.text 와 .count에 대하여 찾아볼 것 -> 아마 textfield 사용에 관해 찾아보면될듯?
+        
+        guard let userInputUnit = Double(userInputValue) else {
+            return
+        } // Measurement 메소드 value 인스턴스에 Double 타입으로 전달해야하므로 타입변환
+        /* Initializer 'init(_:)' requires that 'UITextField' conform to 'StringProtocol' ?? */
+        
+        let userMeters = Measurement(value: userInputUnit, unit: UnitLength.centimeters)
+        
+        let convertUnit = userMeters.converted(to: UnitLength.inches)
+        // convert 와 converted 차이점 찾아보기
+        let result = String(format: "%.21f inch", convertUnit.value)
+        
+        printLabel.text = result
     }
-    
-    
-    
 }
