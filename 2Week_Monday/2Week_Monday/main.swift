4fc36aca04d48b385e2516ede7e9a9b82ed25360
JasonLee0223 <92699723+JasonLee0223@users.noreply.github.com>
2021-11-19 14:45:01 +0900

    Add files via upload
    연습문제4 주민번호 판별검사 업데이트

--- 2Week_Monday/2Week_Monday/main.swift
+++ 2Week_Monday/2Week_Monday/main.swift
@@ -17,4 +17,21 @@ boolArr.printArray(boolOfArray: boolArr.setBoolArray(), newChar: "$")
 
 // 연습문제1 - 마방진
 //resMagic.printMagicSquare(square: resMagic.makeMagicSquare(row: 3))
-resMagic.printMagicSquare(square: resMagic.makeMagicSquare(row: 5))
+//resMagic.printMagicSquare(square: resMagic.makeMagicSquare(row: 5))
+
+// 연습문제6 - 사전(Dictionary) 활용하기
+//print(duplicatedWords(inputWords: ["가을", "우주", "너굴", "우주", "겨울", "봄봄", "너굴", "너굴"]))
+
+// 연습문제5 - 개미 속 퍼즐
+//myAntPuzzle.printAntFunction(antArr: [1], lines: 4)
+
+// 연습문제3 - 비밀번호 등급확인
+/*
+print("Level:", myCheckPW.passwordValidator(password: "onlyEng"))
+print("Level:", myCheckPW.passwordValidator(password: "123456"))
+print("Level:", myCheckPW.passwordValidator(password: "asd123!*"))
+print("Level:", myCheckPW.passwordValidator(password: "asdf1234"))
+*/
+
+// 연습문제4 - 주민번호 판별검사
+//print(practice4.socialNumber(input: "123456-1234567"))
---
+++ 2Week_Monday/2Week_Monday/연습문제2 아이디 규칙 검사.swift
@@ -0,0 +1,21 @@
+//
+//  연습문제2 아이디 규칙 검사.swift
+//  2Week_Monday
+//
+//  Created by 이건행 on 2021/11/10.
+//
+// 5글자 미만이거나 24글자 이상이면 실패
+// 영문 대소문자, 숫자, -만 포함 가능하고 그 외에는 실패
+// 같은 숫자가 연속해서 3회 이상 나오면 실패 111, 222
+// 숫자가 연번으로 3개나오면 실패 123, 234
+// 위 조건을 모두 통과하면 성공
+
+import Foundation
+
+struct PatternIDcheck {
+    func IDValidator(password: String) -> Bool {
+        
+    }
+    
+}
+var myPatternIDcheck = PatternIDcheck
